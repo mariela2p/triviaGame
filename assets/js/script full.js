ae7c6cc047b17db607260277407a622ad960c533
Mariela Pascual <marielapascual@Marielas-MBP.the-beach.net>
2017-11-20 14:12:48 -0500

    mostly html and css

---
+++ assets/js/script full.js
@@ -0,0 +1,107 @@
+var questions = [
+    {
+        gifQuestion: "1.gif",
+        answers: ["The Ring", "Devil's Backbone", "The Sixth Sence", "House of 1000 Corpes"],
+        correctAns: "Devil's Backbone"
+    };
+    {
+        gifQuestion: "2.gif",
+        answers: ["The Plague of Zombies", "Zombie", "Night of the Living Dead", "The City of the Dead"],
+        correctAns: "Night of the Living Dead"
+    };
+    {
+        gifQuestion: "assets/img/3.gif",
+        answers: ["The Cabin in the Woods", "The Host", "28 Days Later", "Get Out"],
+        correctAns: "Get Out"
+    };
+	{
+        gifQuestion: "4.gif",
+        answers: ["The Witch", "Let the Right One In", "New World", "Martyrs"],
+        correctAns: "The Witch"
+    };
+    {
+        gifQuestion: "5.gif",
+        answers: ["Jacob's Ladder", "Misery", "Pet Sematary", "Child's Play"],
+        correctAns: "Pet Sematary"
+    };
+    {
+        gifQuestion: "6.gif",
+        answers: ["Jessabelle", "The Gravedancers", "The Taking of Deborah Logan", "Anguish"],
+        correctAns: "The Taking of Deborah Logan"
+    };
+    {
+        gifQuestion: "7.gif",
+        answers: ["The Ring", "The Grudge", "The Blair Witch Project", "Oculus"],
+        correctAns: "The Ring"
+    };
+    {
+        gifQuestion: "8.gif",
+        answers: ["Night of the Demons", "The Hunger", "Sinister", "HellRaiser"],
+        correctAns: "HellRaiser"
+    };
+    {
+        gifQuestion: "9.gif",
+        answers: ["The Sixth Sence", "The Strangers", "Paranormal Activity", "The Others"],
+        correctAns: "Paranormal Activity"
+    };
+     {
+        gifQuestion: "10.gif",
+        answers: ["Fear the Clowns", "It", "Killjoy", "Poltergeist"],
+        correctAns: "It"
+    };
+    {
+        gifQuestion: "11.gif",
+        answers: ["The Blair Witch Project", "The Tunnel", "As Above, So Bellow", "Evidence"],
+        correctAns: "As Above, So Bellow"
+    };
+     {
+        gifQuestion: "12.gif",
+        answers: ["Rosemary's Baby", "The Exorsist", "Night of the Demon", "Poltergeist"],
+        correctAns: "The Exorsist"
+    };
+    {
+        gifQuestion: "13.gif",
+        answers: ["The Conjuring", "The Haunting", "Oculus", "Darkness"],
+        correctAns: "The Conjuring"
+    };
+    {
+        gifQuestion: "14.gif",
+        answers: ["The Visit", "Insidious", "Ouija", "Sinister"],
+        correctAns: "Sinister"
+    };
+    {
+        gifQuestion: "15.gif",
+        answers: ["The Strangers", "Urban Legend", "Near Dark", "Misery"],
+        correctAns: "Misery"
+    };
+    {
+        gifQuestion: "16.gif",
+        answers: ["From Dusk Till Dawn", "Innocent Blood", "Fright Night", "Planet Terror"],
+        correctAns: "From Dusk Till Dawn"
+    };
+    {
+        gifQuestion: "17.gif",
+        answers: ["The Blackcoat's Daughter", "Lights Out", "The Babadook", "It Follows"],
+        correctAns: "The Babadook"
+    };
+	{
+        gifQuestion: "18.gif",
+        answers: ["The Forest", "The Hallow", "The Antichrist", "The Cabin in the Woods"],
+        correctAns: "The Antichrist"
+    };
+    {
+        gifQuestion: "19.gif",
+        answers: ["Insidious", "The Woman in Black", "The Conjuring 2", "The American Haunting"],
+        correctAns: "Insidious"
+    }{
+        gifQuestion: "20.gif",
+        answers: ["Unrest", "Don't Look Up", "The Bye Bye Man", "It Follows"],
+        correctAns: "It Follows"
+    };
+]
+
+
+
+
+
+    <img src="assets/img/ + questions[0].gif">
---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+var questions = [
+    {
+        gifQuestion: "1.gif",
+        answers: ["The Ring", "Devil's Backbone", "The Sixth Sence", "House of 1000 Corpes"],
+        correctAns: "Devil's Backbone"
+    };
+    {
+        gifQuestion: "2.gif",
+        answers: ["The Plague of Zombies", "Zombie", "Night of the Living Dead", "The City of the Dead"],
+        correctAns: "Night of the Living Dead"
+    };
+    {
+        gifQuestion: "3.gif",
+        answers: ["The Cabin in the Woods", "The Host", "28 Days Later", "Get Out"],
+        correctAns: "Get Out"
+    };
+	{
+        gifQuestion: "4.gif",
+        answers: ["The Witch", "Let the Right One In", "New World", "Martyrs"],
+        correctAns: "The Witch"
+    };
+    {
+        gifQuestion: "5.gif",
+        answers: ["Jacob's Ladder", "Misery", "Pet Sematary", "Child's Play"],
+        correctAns: "Pet Sematary"
+    };
+    {
+        gifQuestion: "6.gif",
+        answers: ["Jessabelle", "The Gravedancers", "The Taking of Deborah Logan", "Anguish"],
+        correctAns: "The Taking of Deborah Logan"
+    };
+    {
+        gifQuestion: "7.gif",
+        answers: ["The Ring", "The Grudge", "The Blair Witch Project", "Oculus"],
+        correctAns: "The Ring"
+    };
+    {
+        gifQuestion: "8.gif",
+        answers: ["Night of the Demons", "The Hunger", "Sinister", "HellRaiser"],
+        correctAns: "HellRaiser"
+    };
+    {
+        gifQuestion: "9.gif",
+        answers: ["The Sixth Sence", "The Strangers", "Paranormal Activity", "The Others"],
+        correctAns: "Paranormal Activity"
+    };
+    {
+        gifQuestion: "10.gif",
+        answers: ["The Blair Witch Project", "The Tunnel", "As Above, So Bellow", "Evidence"],
+        correctAns: "As Above, So Bellow"
+    };
+     {
+        gifQuestion: "11.gif",
+        answers: ["Rosemary's Baby", "The Exorsist", "Night of the Demon", "Poltergeist"],
+        correctAns: "The Exorsist"
+    };
+    {
+        gifQuestion: "12.gif",
+        answers: ["The Conjuring", "The Haunting", "Oculus", "Darkness"],
+        correctAns: "The Conjuring"
+    };
+    {
+        gifQuestion: "13.gif",
+        answers: ["The Visit", "Insidious", "Ouija", "Sinister"],
+        correctAns: "Sinister"
+    };
+    {
+        gifQuestion: "14.gif",
+        answers: ["The Strangers", "Urban Legend", "Near Dark", "Misery"],
+        correctAns: "Misery"
+    };
+    {
+        gifQuestion: "15.gif",
+        answers: ["From Dusk Till Dawn", "Innocent Blood", "Fright Night", "Planet Terror"],
+        correctAns: "From Dusk Till Dawn"
+    };
+	
+]
+
+
+
+
+
+    <img src="assets/img/ + questions[0].gif">
